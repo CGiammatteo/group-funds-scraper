f9b883066710c0942e030ed810a729a6863385f2
Christian Giammatteo <42420087+CGiammatteo@users.noreply.github.com>
2021-08-02 15:31:48 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,21 +8,21 @@ namespace Group_Funds_Finder
     {
         static void Main(string[] args)
         {
-            Console.Title = "Gosty-wosty Group Funds Scraper";
+            Console.Title = "Group Funds Scraper";
             int roEarned = 0;
             int groupsScraped = 0;
 
-            Console.WriteLine("hey HOTTIE, type in 'sexy' to start the group finder");
+            Console.WriteLine("heytype in 'go' to start the group finder");
 
             string answer = Console.ReadLine();
 
-            if(answer == "sexy")
+            if(answer == "go")
             {
-                Console.WriteLine("starting you fucking cunt...");
+                Console.WriteLine("starting...");
             }
             else
             {
-                Console.WriteLine("haha u fucked up, pres a key to exit...");
+                Console.WriteLine("press any key to exit...");
                 Console.ReadKey();
                 
             }
@@ -43,7 +43,7 @@ namespace Group_Funds_Finder
                         {
                             groupsScraped = groupsScraped + 1;
                             roEarned = roEarned + json2.robux;
-                            Console.Title = ("Gosty-wosty Group Funds Scraper | Groups Scraped: " + groupsScraped + " | Robux Earned: " + roEarned);
+                            Console.Title = ("Group Funds Scraper | Groups Scraped: " + groupsScraped + " | Robux Earned: " + roEarned);
                             Console.ForegroundColor = ConsoleColor.Green;
                             Console.WriteLine("Found Robux In Group: " + num);
                             Console.ForegroundColor = ConsoleColor.White;
@@ -57,7 +57,7 @@ namespace Group_Funds_Finder
                         else
                         {
                                 groupsScraped = groupsScraped + 1;
-                            Console.Title = ("Gosty-wosty Group Funds Scraper | Groups Scraped: " + groupsScraped + " | Robux Earned: " + roEarned);
+                            Console.Title = ("Group Funds Scraper | Groups Scraped: " + groupsScraped + " | Robux Earned: " + roEarned);
                             Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("No Robux Found In Group: " + num);
                             Console.ForegroundColor = ConsoleColor.White;
@@ -66,7 +66,7 @@ namespace Group_Funds_Finder
                     else if (json == null)
                     {
                             groupsScraped = groupsScraped + 1;
-                        Console.Title = ("Gosty-wosty Group Funds Scraper | Groups Scraped: " + groupsScraped + " | Robux Earned: " + roEarned);
+                        Console.Title = ("Group Funds Scraper | Groups Scraped: " + groupsScraped + " | Robux Earned: " + roEarned);
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("No Robux Found In Group: " + num);
                         Console.ForegroundColor = ConsoleColor.White;
@@ -74,7 +74,7 @@ namespace Group_Funds_Finder
                     else
                     {
                         groupsScraped = groupsScraped + 1;
-                        Console.Title = ("Gosty-wosty Group Funds Scraper | Groups Scraped: " + groupsScraped + " | Robux Earned: " + roEarned);
+                        Console.Title = ("Group Funds Scraper | Groups Scraped: " + groupsScraped + " | Robux Earned: " + roEarned);
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("No Robux Found In Group: " + num);
                         Console.ForegroundColor = ConsoleColor.White;
@@ -84,7 +84,7 @@ namespace Group_Funds_Finder
                 catch (Exception ex)
                 {
                     groupsScraped = groupsScraped + 1;
-                    Console.Title = ("Gosty-wosty Group Funds Scraper | Groups Scraped: " + groupsScraped + " | Robux Earned: " + roEarned);
+                    Console.Title = ("Group Funds Scraper | Groups Scraped: " + groupsScraped + " | Robux Earned: " + roEarned);
                     Console.ForegroundColor = ConsoleColor.White;
                     Console.WriteLine("ouch?? | Reason: " + ex);
                 }
